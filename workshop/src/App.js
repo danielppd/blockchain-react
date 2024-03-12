e9315914b396297bce523dc87e96c09ead1f16b4
danielppd <daniel.dias@sou.inteli.edu.br>
2024-03-12 16:33:25 -0300

    update

---
+++ workshop/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="border-2 border-red-500">
+      <h1 className="text-bold text-blue-300">Hello, World!</h1>
+      <p className="text-sm text-white text-underline">Olá Inteli</p>
+    </div>
+  );
+}
+
+export default App;
