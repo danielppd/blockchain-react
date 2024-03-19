f21f1ccb0d40fe4cb5451c42c8a1caef8d650096
danielppd <daniel.dias@sou.inteli.edu.br>
2024-03-18 21:04:39 -0300

    Update App.js

--- workshop/src/App.js
+++ workshop/src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import Input from './components/Input';
 import Button from './components/Button';
-import Checkbox from './components/Checkbox';
 
 function App() {
   return (
     /* Container para o formulário como fundo semi transparente*/
 
     <div className="flex min-h-screen w-full h-full items-center justify-center gap-3">
-      <div className="flex flex-col bg-red-600 w-5/6 justify-center items-center ">
+      <div className="flex flex-col bg-red-600 w-3/6 justify-center items-center rounded-md ">
         <p>
-          Receba sua Proof-Of-Attendance NFT
+        Receba sua Proof-Of-Attandance NFT
         </p>
         <p>
-          Para receber sua NFT você precisa ter participado
-          do evento e ter uma carteira na rede Sepolia
+        Para receber sua NFT você precisa ter participado 
+        do evento e ter uma carteira na rede Sepolia.
 
         </p>
 
@@ -35,8 +34,7 @@ function App() {
           type="text"
         />
         <Button type="submit" name="Gerar NFT"/>
-        <Button type="submit" name="maselo"/>
-        <Checkbox veiculo1="Carro" veiculo2="Moto" veiculo3="Bicicleta"></Checkbox>
+
         </form>
       </div>
     </div>
